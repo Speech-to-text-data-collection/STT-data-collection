c3ebea930303f02c9817f6b30810a79025b1d114
DePacifier <milkybekele@gmail.com>
2021-09-13 08:08:08 +0300

    modified server to communicate with kafka as a producer and made upload changes to upload functionality of awsclient

--- scripts/AWSClient.py
+++ scripts/AWSClient.py
@@ -1,6 +1,7 @@
 from logging import log
 import boto3
 import uuid
+from tempfile import SpooledTemporaryFile
 from logger_creator import CreateLogger
 
 logger = CreateLogger('AWSClient', handlers=1)
@@ -21,7 +22,7 @@ class AWSClient():
     def generate_bucket_name(self, bucket_name: str) -> str:
         try:
             generated_name = ''.join([bucket_name, '_', str(uuid.uuid4())])
-            
+
             logger.info(f'GENERATED UNIQUE BUCKET NAME: {generated_name}')
             return generated_name
 
@@ -49,7 +50,7 @@ class AWSClient():
     def delete_bucket(self, bucket_name: str):
         try:
             self.s3_resource.Bucket(bucket_name).delete()
-        
+
             logger.info(f'SUCCESSFULLY DELETED BUCKET: {bucket_name}')
 
         except Exception as e:
@@ -62,12 +63,14 @@ class AWSClient():
             for obj in bucket.objects.all():
                 file_name_list.append(obj.key)
 
-            logger.info(f'{bucket_name} BUCKETS FILE NAMES SUCCESSFULLY FETCHED')
+            logger.info(
+                f'{bucket_name} BUCKETS FILE NAMES SUCCESSFULLY FETCHED')
 
             return file_name_list
 
         except Exception as e:
-            logger.exception(f'FAILED TO RETRIEVE {bucket_name} BUCKETS FILE NAMES')
+            logger.exception(
+                f'FAILED TO RETRIEVE {bucket_name} BUCKETS FILE NAMES')
 
     def get_file_names_with_description(self, bucket_name: str) -> dict:
         try:
@@ -77,47 +80,63 @@ class AWSClient():
             for obj in bucket.objects.all():
                 descriptions = obj.Object()
                 files_dict[obj.key] = {'storage_class': obj.storage_class,
-                                    'last_modified': obj.last_modified,
-                                    'version_id': descriptions.version_id,
-                                    'meta_data': descriptions.metadata
-                                    }
+                                       'last_modified': obj.last_modified,
+                                       'version_id': descriptions.version_id,
+                                       'meta_data': descriptions.metadata
+                                       }
 
-            logger.info(f'{bucket_name} BUCKETS FILE NAMES SUCCESSFULLY FETCHED WITH DESCRIPTIONS')
+            logger.info(
+                f'{bucket_name} BUCKETS FILE NAMES SUCCESSFULLY FETCHED WITH DESCRIPTIONS')
 
             return files_dict
 
         except Exception as e:
-            logger.exception(f'FAILED TO RETRIEVE {bucket_name} BUCKETS FILE NAMES WITH DESCRIPTIONS')
-
+            logger.exception(
+                f'FAILED TO RETRIEVE {bucket_name} BUCKETS FILE NAMES WITH DESCRIPTIONS')
 
     def upload_file(self, bucket_name: str, file_name: str, key: str) -> None:
         try:
             self.s3_resource.Object(bucket_name,
-                                key).upload_file(Filename=file_name)
+                                    key, ExtraArgs={'ACL': 'public-read'}).upload_file(Filename=file_name)
 
-            logger.info(f'{file_name} UPLOADED SUCCESSFULLY TO BUCKET {bucket_name} as {key}')
-        
+            logger.info(
+                f'{file_name} UPLOADED SUCCESSFULLY TO BUCKET {bucket_name} as {key}')
+
         except Exception as e:
             logger.exception('FAILED TO UPLOAD FILE')
+            raise Exception
 
-    def upload_file_bytes(self, data, bucket_name: str, file_name: str):
+    def upload_file_bytes(self, data, bucket_name: str, file_name: str, encode: bool = False):
         try:
-            data = data.encode()
+            if encode:
+                data = data.encode()
             self.s3_client.put_object(Body=data,
-                                  Bucket=bucket_name, Key=file_name)
+                                      Bucket=bucket_name, Key=file_name, ExtraArgs={'ACL': 'public-read'})
 
-            logger.info(f'UPLOADED BYTES SUCCESSFULLY TO BUCKET {bucket_name} as {file_name}')
+            logger.info(
+                f'UPLOADED BYTES SUCCESSFULLY TO BUCKET {bucket_name} as {file_name}')
 
         except Exception as e:
             logger.exception('FAILED TO UPLOAD BYTES')
+
+    def upload_file_object(self, temp_file: SpooledTemporaryFile, bucket_name: str, file_name: str):
+        try:
+            self.s3_client.upload_fileobj(
+                Fileobj=temp_file, Bucket=bucket_name, Key=file_name, ExtraArgs={'ACL': 'public-read'})
+
+            logger.info(
+                f'UPLOADED FILE OBJECT SUCCESSFULLY TO BUCKET {bucket_name} as {file_name}')
+
+        except Exception as e:
+            logger.exception('FAILED TO UPLOAD FILE OBJECT')
 
     def get_file_link(self, bucket_name: str, file_name: str) -> str:
         try:
             bucket_location = self.s3_client.get_bucket_location(
                 Bucket=bucket_name)
-            object_url = "https://s3-{0}.amazonaws.com/{1}/{2}".format(
-                bucket_location['LocationConstraint'],
+            object_url = "https://{0}.s3.{1}.amazonaws.com/{2}".format(
                 bucket_name,
+                bucket_location['LocationConstraint'],
                 file_name)
 
             logger.info(f'SUCCESSFULLY GENERATED FILE LINK')
@@ -130,7 +149,7 @@ class AWSClient():
     def load_file_bytes(self, bucket_name: str, file_name: str):
         try:
             s3_object = self.s3_client.get_object(
-            Bucket=bucket_name, Key=file_name)
+                Bucket=bucket_name, Key=file_name)
             data = s3_object['Body'].read()
 
             logger.info(f'SUCCESSFULLY LOADED FILE {file_name} AS BYTES')
@@ -143,10 +162,10 @@ class AWSClient():
     def download_file(self, bucket_name: str, file_name: str, location: str = '/tmp/'):
         try:
             self.s3_resource.Object(bucket_name, file_name).download_file(
-            f'{location}{file_name}')
+                f'{location}{file_name}')
 
             logger.info(f'{file_name} SUCCESSFULLY DOWNLOAD TO {location}')
-        
+
         except Exception as e:
             logger.exception('FAILED TO DOWNLOAD FILE')
 
@@ -154,11 +173,11 @@ class AWSClient():
         try:
             self.s3_resource.Object(bucket_name, file_name).delete()
 
-            logger.info(f'{file_name} SUCCESSFULLY DELETED FROM {bucket_name} BUCKET')
+            logger.info(
+                f'{file_name} SUCCESSFULLY DELETED FROM {bucket_name} BUCKET')
 
         except Exception as e:
             logger.exception('FAILED TO DELETE FILE FROM BUCKET')
-
 
     def delete_all_files_From_bucket(self, bucket_name: str):
         try:
@@ -170,7 +189,9 @@ class AWSClient():
 
             bucket.delete_objects(Delete={'Objects': res})
 
-            logger.info(f'SUCCESSFULLY DELETED ALL FILES IN {bucket_name} BUCKET')
+            logger.info(
+                f'SUCCESSFULLY DELETED ALL FILES IN {bucket_name} BUCKET')
 
         except Exception as e:
-            logger.exception(f'FAILED TO DELETE ALL FILES IN {bucket_name} BUCKET')
+            logger.exception(
+                f'FAILED TO DELETE ALL FILES IN {bucket_name} BUCKET')
--- scripts/server.py
+++ scripts/server.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, File, UploadFile
 from KafkaClient import KafkaClient
+from AWSClient import AWSClient
+from datetime import datetime, timezone
+
+# Create an AWSClient to upload files to S3 Bucket
+server_aws_client = AWSClient()
 
 # Create Consumer to read Text Corpus Values from the Kafka Cluster
-## Instantiating a KafkaClient Object
+# Instantiating a KafkaClient Object
 server_kafka_consumer = KafkaClient(
     'python-fast-api-server',
     [
@@ -10,8 +15,8 @@ server_kafka_consumer = KafkaClient(
         'localhost:9093',
         'localhost:9094'
     ]
-    )
-## Creating a consumer using the kafkaclient
+)
+# Creating a consumer using the kafkaclient with a json deserializer
 server_kafka_consumer.create_consumer(
     topics='Text-Corpus',
     offset='earliest',
@@ -19,32 +24,76 @@ server_kafka_consumer.create_consumer(
     group_id='abebe',
     value_deserializer=server_kafka_consumer.get_json_deserializer(),
     timeout=1000
-    )
+)
 
-## Function which reads using the created consumer
+# Function which reads using the created consumer
+
+
 async def call_consumer_get_data():
     return server_kafka_consumer.get_data()
 
+# Creating a producer using the kafkaclient with a json serializer
+server_kafka_consumer.create_producer(
+    value_serializer=server_kafka_consumer.get_json_serializer())
 
-# Create Producer to Upload audio file to S3 Bucket and pass the link to kafka servers
-
-
-
-
-
-# suppose_values = [
-#     {'id': 1, 'text': 'I am working on the server'},
-#     {'id': 2, 'text': 'I am flabergased'},
-#     {'id': 3, 'text': 'I am milky'},
-#     {'id': 4, 'text': 'I am laughing'},
-#     {'id': 5, 'text': 'I am joking'},
-#     {'id': 6, 'text': 'I am playing'},
-#     {'id': 7, 'text': 'I am shoveling'}
-# ]
+# Function called when file is recieved by server
 
 
-# async def getvalues():
-#     return suppose_values
+async def validate_file(file: UploadFile) -> bool:
+    try:
+        # Check file is an audio type
+        assert file.content_type.startswith('audio')
+
+        return True
+
+    except AssertionError:
+        return False
+
+    except Exception as e:
+        print('FAILED TO VALIDATE FILE')
+        print(e)
+        # return False
+
+
+async def upload_file(file: UploadFile) -> bool:
+    try:
+        server_aws_client.upload_file_object(
+            file.file, 'unprocessed-stt-audio', file.filename)
+
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
+
+
+def generate_file_data(file: UploadFile):
+    file_link = server_aws_client.get_file_link(
+        'unprocessed-stt-audio', file.filename)
+
+    text_id, audio_id = file.filename.split('_')
+
+    upload_time = datetime.now(timezone.utc)
+    date = upload_time.strftime("%m/%d/%y")
+    time = upload_time.strftime("%H:%M:%S")
+
+    data = {'file_name': file.filename, 'content_type': file.content_type, 'text_id': text_id, 'audio_id': audio_id,
+            'link': file_link, 'upload_date (UTC)': date, 'upload_time (UTC)': time}
+
+    return data
+
+# Using  Upload audio file to S3 Bucket and pass the link to kafka servers
+
+
+async def send_detail_to_kafka(file: UploadFile):
+    try:
+        file_data = generate_file_data(file)
+
+        server_kafka_consumer.send_data('Text-Audio-input', [file_data])
+
+    except Exception as e:
+        print(e)
+
 
 # List for holding fetched text values
 fetched_data = []
@@ -57,7 +106,7 @@ app = FastAPI()
 async def fetch_text():
     # Check if we have previously fetched data with more than 20 data objects
     if(len(fetched_data) <= 50):
-        print('Items lower than 5, fetching data')
+        print('Items lower than 50, fetching data')
         # Fetch Data
         data = await call_consumer_get_data()
         fetched_data.extend(data)
@@ -73,17 +122,18 @@ async def fetch_text():
 @app.post('/upload-audio')
 async def handle_upload_audio(file: UploadFile = File(...)):
     try:
-        # filename give you myimage.jpg
-        # content_type gives image/jpeg
-        # file gives us a spooledtemporaryfile(file like object)
-
-        # Upload file to S3 Bucket
-
-        # Send Data to Kafka the text id and reference link from S3 using the producer
+        # Validate if file is audio
+        if(await validate_file(file)):
+            # Upload file to S3 Bucket
+            assert await upload_file(file)
+            # Send Data to Kafka the text id and reference link from S3 using the producer
+            await send_detail_to_kafka(file)
 
         # return Success or Failure
-        # return {'filename': file.filename, 'content_type': file.content_type}
-        return {'filename': file.filename, 'content_type': file.content_type, 'status': 'success'}
+        return {'filename': file.filename, 'content_type': file.content_type, 'status': 'success', 'detail': 'File Upload Successful'}
+
+    except AssertionError:
+        return {'filename': file.filename, 'content_type': file.content_type, 'status': 'failure', 'detail': 'File Upload Failed'}
 
     except Exception as e:
         print(e)
